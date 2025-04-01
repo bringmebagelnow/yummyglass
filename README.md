fc7e4f885c5a2af473dab852304303f49a507a3c
BringMeBagel <shergon23@yandex.ru>
2025-04-01 07:51:01 +0300

    start fresh

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-github is confusing
-if you want to look at the source i mean be my guest but yikes i apologise in advance
+~~github is confusing
+if you want to look at the source i mean be my guest but yikes i apologise in advance~~
+#GUESS WHAT
+###we're back babyyyyy
+###eat glass NOW (but better)
---
+++ fabric/src/client/java/bringmebagel/yummyglass/YummyGlassClient.java
@@ -0,0 +1,10 @@
+package bringmebagel.yummyglass;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class YummyGlassClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
